39b9a9f4a2537a2132d607ee48cffce21a52871d
MIR HB RAHMAN <ahadcr0@gmail.com>
2022-09-11 23:44:53 +0600

    Added typescript to user model

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,5 +1,21 @@
 import mongoose from "mongoose";
 
+// For attributes
+interface UserAttrs{
+    email: string,
+    password: string
+}
+// For model structure
+interface UserModel extends mongoose.Model<any>{
+    build(attrs: UserAttrs): UserDoc;
+}
+// Properties of user model
+interface UserDoc extends mongoose.Document{
+    email: string,
+    password: string
+}
+
+
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
@@ -11,7 +27,11 @@ const userSchema = new mongoose.Schema({
     }
 });
 
+// Just for typescript check
+userSchema.statics.build = (attrs: UserAttrs)=>{
+    return new User(attrs);
+}
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
 
 export { User };
