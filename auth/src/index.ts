5b3dde671a9cf8dd1ca96f316e8ab5c8f2e6fc4a
MIR HB RAHMAN <ahadcr0@gmail.com>
2022-09-11 23:23:31 +0600

    Added mongodb for auth

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
+
 import { currentUserRouter } from './routes/current-user';
 import { signupRouter } from './routes/signup';
 import { signinRouter } from './routes/signin';
@@ -22,6 +24,17 @@ app.all('*', ()=>{
 
 app.use(errorHandler);
 
+const start = async()=>{
+    try{
+        await mongoose.connect('mongodb://auth-mongo-srv:27017');
+        console.log('Connected to mongoDB');
+    }catch(err){
+        console.log(err);
+    }
+}
+
 app.listen(3000, ()=>{
     console.log('Listening on port 3000!');
 });
+
+start();
