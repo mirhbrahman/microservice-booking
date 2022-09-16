0a68a723d6d3945affde149d77cdd41f9542acdc
MIR HB RAHMAN <ahadcr0@gmail.com>
2022-09-16 20:18:48 +0600

    Format return object, Signup completed

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -25,6 +25,15 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     }
+}, {
+    toJSON: {
+        transform(doc, ret) {
+            ret.id = ret._id;
+            delete ret._id;
+            delete ret.password;
+            delete ret.__v;
+        },
+    }
 });
 
 // Hashed password before save
