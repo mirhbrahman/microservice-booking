8a4cbc42d03fabb8681d022d5fe684fe4548f09f
MIR HB RAHMAN <ahadcr0@gmail.com>
2022-10-06 23:23:50 +0600

    Adding signup page at client

---
+++ client/pages/auth/signup.js
@@ -0,0 +1,35 @@
+import { useState } from "react"
+
+export default () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        console.log(email, password)
+    }
+
+    return (
+        <form onSubmit={onSubmit}>
+            <h1>Sign Up</h1>
+            <div className="form-group">
+                <label>Email Address</label>
+                <input
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                    className="from-control"
+                />
+            </div>
+            <div className="form-group">
+                <label>Password</label>
+                <input
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                    type="password"
+                    className="from-control"
+                />
+            </div>
+            <button>Sign up</button>
+        </form>
+    )
+}
