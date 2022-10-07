7b3d4a8282a8d46f5e672a90f7c50af7db5b4d95
MIR HB RAHMAN <ahadcr0@gmail.com>
2022-10-07 23:10:32 +0600

    On success redirect to hobe via callback

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import axios from 'axios'
+import Router from "next/router";
 import useRequest from "../../hooks/use-request";
 
 export default () => {
@@ -10,6 +10,9 @@ export default () => {
         method: 'post',
         body: {
             email, password
+        },
+        onSuccess: () => {
+            Router.push('/')
         }
     })
 
